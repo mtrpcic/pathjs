266173333f500688b5cf6b1a16d6b08d0fff7b86
Igor Lima <igor.lima@evolut.io>
2013-05-23 02:17:55 -0300

    Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '.'
+        }
+      }
+    },
+
+    webdriver: {
+      dev: {
+        options: {
+          logLevel: 'silent',
+          browser: 'firefox',
+          binary: null
+        },
+        url: 'http://localhost:9001/tests/path.js.test.html',
+        tests: ['./tests/spec/pathjsSpec.js']
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'path.min.js': ['path.js']
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        "globals": {
+          "exports": true,
+          "buster": true
+        }
+      },
+      all: ['Gruntfile.js', 'path.js', 'tests/spec/**/*.js']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-webdriver');
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'connect', 'webdriver']);
+
+};
